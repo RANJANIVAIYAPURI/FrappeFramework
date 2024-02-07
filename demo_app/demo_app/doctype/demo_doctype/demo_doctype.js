b564622835473f473e0c0dc418b222cb9872c845
RANJANIVAIYAPURI <ranjaniv1999@gmail.com>
2024-02-07 14:13:15 +0530

    init

---
+++ demo_app/demo_app/doctype/demo_doctype/demo_doctype.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, DEMO and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Demo DocType", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ demo_app/demo_app/doctype/firstdocument/firstdocument.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, DEMO and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("FirstDocument", {
+// 	refresh(frm) {
+
+// 	},
+// });
